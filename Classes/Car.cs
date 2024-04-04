2c95076a1acf00f561581847cbcb858184390b05
Robert Schofield <rschofield1992@gmail.com>
2024-04-04 15:33:43 -0400

    commit

---
+++ Classes/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+
+
+
+        //step2-- Create a new class, create properties of Make,Model,year
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public Car()
+		{
+		}
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,20 +1,20 @@
 ﻿namespace Classes
 {
 
-    public class Car
-    {
+    //public class Car
+    //{
 
-        //step2-- Create a new class, create properties of Make,Model,year
-        public string Make { get; set; }
+    //    //step2-- Create a new class, create properties of Make,Model,year
+    //    public string Make { get; set; }
 
-        public string Model { get; set; }
+    //    public string Model { get; set; }
 
-        public int Year { get; set; }
-
-
+    //    public int Year { get; set; }
 
 
-    }
+
+
+    //}
     public class Program
     {
 
