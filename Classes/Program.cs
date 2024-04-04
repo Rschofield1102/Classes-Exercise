b3f6c6a3217e07b92f1d28b9aade0138b5e08261
Robert Schofield <rschofield1992@gmail.com>
2024-04-04 11:45:23 -0400

    commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,34 +1,54 @@
 ﻿namespace Classes
 {
+
+    public class Car
+    {
+
+        //step2-- Create a new class, create properties of Make,Model,year
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+
+
+
+    }
     public class Program
-    {   //step1-- Create a public class called Car
-        public class Car
-        { 
+    {
+
+
+        static void Main(string[] args)
+
+        {   //Step3-- In main Create a new instance of car Class
+
+
+            Car myCar = new Car();
+
+            ////---create instance and set values in one
+            //Car myCar = new Car() { Make = "Toyota", Model ="Corolla", Year= 2001};
+            ///
+
+
+            //Step4-- Set values to the properties for the object
+            //----THIS is DOT NOTATION
+
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2001;
             
-          //step2-- inside of the class, create Make,Model,year
-            public  string Make { get; set; } 
 
-            public string Model { get; set; }
-
-            public int Year { get; set; }
-
-            static void Main(string[] args)
-            {   //Step3-- In main Create a new instance of car Class
-                Car myCar = new Car(); 
-                //Step4-- Set values to the properties for the object
-                myCar.Make = "Toyota";
-                myCar.Model = "Corolla";
-                myCar.Year = 2001;
-
-                //Step4--Print to the console.
-                Console.WriteLine(myCar.Make); 
-                Console.WriteLine(myCar.Model);
-                Console.WriteLine(myCar.Year);
+            //Step4--Print to the console, to see the properties
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
 
 
-            }
-          
 
         }
+
+
     }
+    
 }
